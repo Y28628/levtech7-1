a3fa780bc1a3b6c9ff79f5d263b63a80ab8e57ef
Y28628 <yuya.620kouki416@icloud.com>
2024-04-16 01:24:11 +0900

    　levtech8-2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,9 +14,10 @@
             @foreach($posts as $post)
             <div class="post">
                 <h2 class="title">{{$post->title}}</h2>
-                <p class="body">{{$post->body}}</p>
+                <p class="body">{{ $post->body }}</p>
             </div>
             @endforeach
         </div>
+        <div class='paginate'>{{ $posts->links()}}</div>
     </body>
 </html>
